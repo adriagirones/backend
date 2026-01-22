2221373e32b01548c2e7a55899f0922e00200e1f
adriagirones <67000134+adriagirones@users.noreply.github.com>
2026-01-22 11:58:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Backend running');
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+/**
+ * ROUTE ROOT (OBLIGATORIA)
+ */
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+/**
+ * LISTEN (OBLIGATORIO 0.0.0.0)
+ */
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
+});
